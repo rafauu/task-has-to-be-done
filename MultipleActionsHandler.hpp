46622f8137bffad1a26dfc061132d822ac006f55
rafauu <gliniakr@gmail.com>
2019-07-23 00:53:33 +0200

    vector replaced with unordered_map

--- MultipleActionsHandler.hpp
+++ MultipleActionsHandler.hpp
@@ -1,5 +1,6 @@
-#include <vector>
-#include <algorithm>
+#include <boost/algorithm/cxx11/all_of.hpp>
+#include <boost/range/adaptor/map.hpp>
+#include <unordered_map>
 
 template <typename Type>
 class MultipleActionsHandler
@@ -8,32 +9,25 @@ public:
     template <typename... Args>
     constexpr explicit MultipleActionsHandler(Args&&... args)
     {
-        (holder.emplace_back(Action{std::forward<Args>(args), false}), ...);
+        (holder.emplace(std::forward<Args>(args), false), ...);
     }
 
     void markAsDone(const Type& id)
     {
-        auto it = std::find_if(holder.begin(),
-                               holder.end(),
-                               [&](auto&& action){ return action.id == id; });
-        if (it != holder.end())
-        {
-            it->isDone = true;
-        }
+        holder.insert_or_assign(id, true);
+    }
+
+    void markAllAsDone()
+    {
+        for(auto& id : holder)
+            holder[id] = true;
     }
 
     bool isHandlingFinished() const
     {
-        return std::all_of(holder.cbegin(),
-                           holder.cend(),
-                           [](auto&& action){ return action.isDone; });
+        return boost::algorithm::all_of_equal(holder | boost::adaptors::map_values, true);
     }
 
 private:
-    struct Action
-    {
-        Type id;
-        bool isDone;
-    };
-    std::vector<Action> holder;
+    std::unordered_map<Type, bool> holder;
 };
