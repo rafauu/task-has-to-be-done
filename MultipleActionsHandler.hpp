264db35f557bcceafea3544814fe9a09313e028b
rafauu <gliniakr@gmail.com>
2019-08-03 00:09:28 +0200

    Implementation of handler separated from declarations

--- MultipleActionsHandler.hpp
+++ MultipleActionsHandler.hpp
@@ -3,37 +3,50 @@
 #include <unordered_map>
 #include <iostream>
 
+
 template <auto... Args>
 class MultipleActionsHandler
 {
-using Type = std::decay_t<std::common_type_t<decltype(Args)...>>;
+private:
+    using Type = std::decay_t<std::common_type_t<decltype(Args)...>>;
 
 public:
-    constexpr explicit MultipleActionsHandler()
-    {
-        (holder.emplace(std::forward<Type>(Args), false), ...);
-    }
-
-    void markAsDone(Type id)
-    {
-        holder.insert_or_assign(id, true);
-        std::cout << id << " marked as done" << std::endl;
-    }
-
-    void markAllAsDone()
-    {
-        for(auto&& [id, isDone] : holder)
-        {
-            isDone = true;
-            std::cout << id << " marked as done" << std::endl;
-        }
-    }
-
-    bool isHandlingFinished() const
-    {
-        return boost::algorithm::all_of_equal(holder | boost::adaptors::map_values, true);
-    }
+    constexpr MultipleActionsHandler();
+    void markAsDone(Type id);
+    void markAllAsDone();
+    bool isHandlingFinished() const;
 
 private:
     std::unordered_map<Type, bool> holder;
 };
+
+
+template <auto... Args>
+constexpr MultipleActionsHandler<Args...>::MultipleActionsHandler()
+{
+    (holder.emplace(std::forward<Type>(Args), false), ...);
+}
+
+template <auto... Args>
+void MultipleActionsHandler<Args...>::markAsDone(Type id)
+{
+    holder.insert_or_assign(id, true);
+    std::cout << id << " marked as done" << std::endl;
+}
+
+template <auto... Args>
+void MultipleActionsHandler<Args...>::markAllAsDone()
+{
+    for(auto&& [id, isDone] : holder)
+    {
+        isDone = true;
+        std::cout << id << " marked as done" << std::endl;
+    }
+}
+
+template <auto... Args>
+bool MultipleActionsHandler<Args...>::isHandlingFinished() const
+{
+    return boost::algorithm::all_of_equal(holder | boost::adaptors::map_values, true);
+}
+
